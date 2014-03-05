46fe2ed26bc20103d8004f92020d347e5cb477a7
Jess Kenney <jessk@mit.edu>
2014-03-05 00:54:30 -0500

    initial try at data loading

--- vis.js
+++ vis.js
@@ -20,3 +20,42 @@ $(document).ready(function(){
         $(this).find(".description").hide()
     })
 })
+
+
+function load_data(data) {
+    // add metas, intentions, elements
+    categories = ["metas", "intentions", "elements"]
+    for (int c = 0; c < 3; c++) {
+        cat = categories[c]
+        for (int e=0; e<data["all-"+cat].length; e++) {
+            element = data["all-"+cat][e]
+            object = $("<span>")
+                .addClass("label")
+                .addClass("is-"+element)
+                .html(element)
+            $("."+cat).append(object)
+        }
+    }
+
+    for (int e=0; e<data["blog_entries"].length; e++) {
+        author = data["blog_entries"][e]["author"]
+        post_url = data["blog_entries"][e]["post_url"]
+        object = $("<span>")
+            .addClass("label")
+            .addClass("is-"author)
+            .html(author)
+        for (int c = 0; c < 3; c++) {
+            cat = categories[c]
+            labels = data["blog_entries"][e][cat]
+            for (int l=0; l<labels.length; l++){
+                label = labels[l]
+                object.addClass("tags-"+label)
+                $(".is-"+label).addClass("tags-"+author)
+            }
+        }
+        $(".author").append(object)
+    }
+
+
+
+}
