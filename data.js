870c30d0567d0a03d6bd76154a5b90b031c71f0e
Miles Steele <miles@milessteele.com>
2014-03-05 00:54:11 -0500

    add data.js

---
+++ data.js
@@ -0,0 +1,69 @@
+window.sweet_data = {
+  "all_metas": [
+    "strict",
+    "loose",
+    "contradictory"
+  ],
+  "all_intentions": [
+    "factual",
+    "observational",
+    "poetic",
+    "persuasive",
+    "subjective",
+    "objective",
+    "deliberate",
+    "non-deliberate"
+  ],
+  "all_elements": [
+    "facts",
+    "social-political",
+    "story",
+    "technology",
+    "style",
+    "argument",
+    "artistic"
+  ],
+  "blog_entries": [
+    {
+      "author": "Fitzsimons, Laughren, Williamson",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/australizan-documentary-trish.html?view=classic",
+      "elements": [
+        "artistic",
+        "technology"
+      ],
+      "metas": [
+        "strict"
+      ],
+      "intentions": [
+        "deliberate"
+      ]
+    },
+    {
+      "author": "Hediger, Vonderau",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/?view=classic",
+      "elements": [
+        "style"
+      ],
+      "metas": [
+        "loose"
+      ],
+      "intentions": [
+        "deliberate",
+        "objective"
+      ]
+    },
+    {
+      "author": "Goldsmith",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/whatever-current-definition-there.html?view=classic",
+      "elements": [
+        "facts"
+      ],
+      "metas": [
+        "loose"
+      ],
+      "intentions": [
+        "deliberate"
+      ]
+    }
+  ]
+};
