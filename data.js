1ea9d8f4aa6de512ee73e35834d471522c24b926
Brandon Vasquez <bvasquez@mit.edu>
2014-03-05 01:51:43 -0500

    added data

--- data.js
+++ data.js
@@ -34,8 +34,7 @@ window.sweet_data = {
       "metas": [
         "strict"
       ],
-      "intentions": [
-        "deliberate"
+      "intentions": [ "deliberate"
       ]
     },
     {
@@ -64,6 +63,131 @@ window.sweet_data = {
       "intentions": [
         "deliberate"
       ]
-    }
+    },
+>>>    {
+      "author": "Hampe",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/making-documentary-films-and-reality.html?view=classic",
+      "elements": [
+        "technology"
+      ],
+      "metas": [
+        "strict"
+      ],
+      "intentions": [
+        "deliberate",
+        "objective"
+      ]
+    },
+    {
+    "author": "Ellis, Betsy",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/a-new-history-of-documentary-film-jack.html?view=classic",
+      "elements": [
+        "facts",
+        "technology",
+        "artistic",
+        "style",
+        "social-political"
+      ],
+      "metas": [
+        "strict"
+      ],
+      "intentions": [
+        "deliberate"
+      ]
+    },
+    {
+    "author": "Beattie",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/documentary-screens-keith-beattie-2004.html?view=classic",
+      "elements": [
+        "facts",
+        "social-political",
+        "argument"
+      ],
+      "metas": [
+        "contradiction"
+      ],
+      "intentions": [
+        "factual",
+        "deliberate"
+      ]
+    },
+    {
+    "author": "Hogarth",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/realer-than-reel-david-hogarth-2006.html?view=classic",
+      "elements": [
+        "technology",
+        "social-political"
+      ],
+      "metas": [
+        "loose"
+      ],
+      "intentions": [
+        "persuasive"
+      ]
+    },
+    {
+    "author": "Hogarth",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/realer-than-reel-david-hogarth-2006.html?view=classic",
+      "elements": [
+        "technology",
+        "social-political"
+      ],
+      "metas": [
+        "loose"
+      ],
+      "intentions": [
+        "persuasive"
+      ]
+    },
+    {
+    "author": "Ellis",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/documentary-witness-and-self-revelation.html?view=classic",
+      "elements": [
+        "argument",
+        "facts",
+        "story",
+        "style"
+      ],
+      "metas": [
+        "contradictory"
+      ],
+      "intentions": [
+        "objective",
+        "observational",
+        "persuasive",
+        "deliberate"
+      ]
+    },
+    {
+    "author": "Benyahia",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/teaching-film-and-tv-documentary-sarah.html?view=classic",
+      "elements": [
+        "facts",
+        "story"
+      ],
+      "metas": [
+        "loose"
+      ],
+      "intentions": [
+        "objective",
+        "persuasive",
+        "deliberate"
+      ]
+    },
+    {
+    "author": "Macdonald, Cousins",
+      "post_url": "http://johngriersonhasalottoanswerfor.blogspot.com/2014/02/imagining-reality-kevin-macdonald-and.html?view=classic",
+      "elements": [
+        "story"
+      ],
+      "metas": [
+        "loose"
+      ],
+      "intentions": [
+        "objective",
+        "observational"
+      ]
+    },
+
   ]
 };
